375d0232e79236591e5e2b0463e731e4895a9996
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-16 13:29:12 +0500

    completed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,12 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str != null && str.length() > 0 && str.charAt(0) == '0')
+            return false;
+        return NumberUtils.toInt(str) > 0
+                || NumberUtils.toDouble(str) > 0;
     }
 }
